fd83bfa8edf9de8cb914fdd0eecc911856aa3cd6
KETHA-DIVYA-SREE <65858609+KETHA-DIVYA-SREE@users.noreply.github.com>
2022-10-01 22:48:17 +0530

    2 files modified

--- front-end/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ front-end/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -9,9 +10,17 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
   recipe!: Recipe;
+  id!: number;
 
-  constructor(private _recipeService: RecipeService) {}
-  ngOnInit(): void {}
+  constructor(
+    private _recipeService: RecipeService,
+    private _route: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    this._route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+    });
+  }
 
   onAddToShoppingList() {
     this._recipeService.addIngreientsToShoppingList(this.recipe.ingredients);
