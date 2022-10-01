e4d12394d8574df9d4122a3f31f0fca8d2914707
KETHA-DIVYA-SREE <65858609+KETHA-DIVYA-SREE@users.noreply.github.com>
2022-10-01 19:24:14 +0530

    8 files modified

--- front-end/src/app/recipes/recipe-list/recipe-list.component.ts
+++ front-end/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -19,7 +19,12 @@ export class RecipeListComponent implements OnInit {
       'https://img.freepik.com/premium-photo/homemade-indian-flat-bread-chapati-roti-made-with-butter-ghee-flour-water_627828-443.jpg?w=360'
     ),
   ];
-  constructor() {}
+  @Output() onRecipeElSelected = new EventEmitter<Recipe>();
 
+  constructor() {}
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.onRecipeElSelected.emit(recipe);
+  }
 }
