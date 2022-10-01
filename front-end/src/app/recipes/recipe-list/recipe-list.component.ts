cbce6f9c821471acfd94ee414586a4c07df20940
KETHA-DIVYA-SREE <65858609+KETHA-DIVYA-SREE@users.noreply.github.com>
2022-10-01 16:39:12 +0530

    3 files modified, 1 added

--- front-end/src/app/recipes/recipe-list/recipe-list.component.ts
+++ front-end/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Recipe1',
+      'Description1',
+      'https://thumbs.dreamstime.com/b/fried-cutlet-burger-vegetables-black-plate-wooden-background-top-view-free-space-your-text-flat-lay-164637219.jpg'
+    ),
+    new Recipe(
+      'Recipe2',
+      'Description2',
+      'https://img.freepik.com/premium-photo/homemade-indian-flat-bread-chapati-roti-made-with-butter-ghee-flour-water_627828-443.jpg?w=360'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
