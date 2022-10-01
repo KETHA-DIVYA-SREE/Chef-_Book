88544b1df1640602d3426637f0136996cc1db41a
KETHA-DIVYA-SREE <65858609+KETHA-DIVYA-SREE@users.noreply.github.com>
2022-10-01 20:21:42 +0530

    3 files modified

--- front-end/src/app/recipes/recipe-list/recipe-list.component.ts
+++ front-end/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,22 +8,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Recipe1',
-      'Description1',
-      'https://thumbs.dreamstime.com/b/fried-cutlet-burger-vegetables-black-plate-wooden-background-top-view-free-space-your-text-flat-lay-164637219.jpg'
-    ),
-    new Recipe(
-      'Recipe2',
-      'Description2',
-      'https://img.freepik.com/premium-photo/homemade-indian-flat-bread-chapati-roti-made-with-butter-ghee-flour-water_627828-443.jpg?w=360'
-    ),
-  ];
+  recipes!: Recipe[];
   @Output() onRecipeElSelected = new EventEmitter<Recipe>();
 
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(private _recipeService: RecipeService) {}
+  ngOnInit(): void {
+    this.recipes = this._recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.onRecipeElSelected.emit(recipe);
--- front-end/src/app/recipes/recipe.service.ts
+++ front-end/src/app/recipes/recipe.service.ts
@@ -1,9 +1,28 @@
+import { EventEmitter } from '@angular/core';
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private _recipes: Recipe[] = [
+    new Recipe(
+      'Recipe1',
+      'Description1',
+      'https://thumbs.dreamstime.com/b/fried-cutlet-burger-vegetables-black-plate-wooden-background-top-view-free-space-your-text-flat-lay-164637219.jpg'
+    ),
+    new Recipe(
+      'Recipe2',
+      'Description2',
+      'https://img.freepik.com/premium-photo/homemade-indian-flat-bread-chapati-roti-made-with-butter-ghee-flour-water_627828-443.jpg?w=360'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getRecipes() {
+    return this._recipes.slice();
+  }
 }
