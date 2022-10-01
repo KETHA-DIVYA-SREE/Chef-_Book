24b73e470a1037f3ef219bd1b724fd107c363aa2
KETHA-DIVYA-SREE <65858609+KETHA-DIVYA-SREE@users.noreply.github.com>
2022-10-01 21:53:48 +0530

    4 files modified

--- front-end/src/app/recipes/recipe.service.ts
+++ front-end/src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../models/ingredient.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,12 +12,20 @@ export class RecipeService {
     new Recipe(
       'Recipe1',
       'Description1',
-      'https://thumbs.dreamstime.com/b/fried-cutlet-burger-vegetables-black-plate-wooden-background-top-view-free-space-your-text-flat-lay-164637219.jpg'
+      'https://thumbs.dreamstime.com/b/fried-cutlet-burger-vegetables-black-plate-wooden-background-top-view-free-space-your-text-flat-lay-164637219.jpg',
+      [
+        new Ingredient('Recipe-1-Ingredient-1', 1),
+        new Ingredient('Recipe-1-Ingredient-2', 2),
+      ]
     ),
     new Recipe(
       'Recipe2',
       'Description2',
-      'https://img.freepik.com/premium-photo/homemade-indian-flat-bread-chapati-roti-made-with-butter-ghee-flour-water_627828-443.jpg?w=360'
+      'https://img.freepik.com/premium-photo/homemade-indian-flat-bread-chapati-roti-made-with-butter-ghee-flour-water_627828-443.jpg?w=360',
+      [
+        new Ingredient('Recipe-2-Ingredient-1', 1),
+        new Ingredient('Recipe-2-Ingredient-2', 2),
+      ]
     ),
   ];
 
